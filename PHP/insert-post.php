a2056e5faabc971744402e25d91f70b1e3166d49
MedyOsef <porgomedyfaycal@gmail.com>
2021-04-21 14:26:49 +0000

    Ajout d'un editeur de texte tinymce

--- PHP/insert-post.php
+++ PHP/insert-post.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
     <meta charset="UTF-8">
@@ -11,6 +11,7 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
     <!--Lien fichers css et autre-->
     <!-- <link rel="icon" type="image/png" href="../assets/Logos/logomedy.png"> -->
+    <script src="https://cdn.tiny.cloud/1/2fq7dtauax2tzjs2axcj0shvm1kwd675899u6ux5p6yvqrzd/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
     <link rel="stylesheet" href="../CSS/reset.css">
     <link rel="stylesheet" href="../CSS/footer.css">
     <link rel="stylesheet" href="../CSS/insert-post.css">
@@ -28,11 +29,9 @@
     <div class="insert-post-contenaire">
         <div class="post-editor">
             <input class="title" type="text" name="title" placeholder="Titre">
-            <div class="editor-customizer">
-
-            </div>
+            <!-- <div class="editor-customizer"></div> -->
             <div class="textarea-contenaire">
-                <textarea class="textarea" name="" cols="95" rows="31"></textarea>
+                <textarea id="editor" class="text-editor" name="" rows="30"></textarea>
             </div>
         </div>
 
@@ -43,16 +42,34 @@
             </div>
             <h3>Paramètres des articles</h3>
             <div class="settings">
-                <a href="">Categorie</a>
-                <hr>
-                <a href="">Libellés</a>
-                <hr>
-                <a href="">Date de publication</a>
-                <hr>
+                    <input id="tag" type="text" name="tag" placeholder="Libellés">
+    
+                    <h4 class="date-title">Date de publication</h4>
+                    <input id="date-of-the-publication" type="date" name="date-of-the-publication">
+                    
+                    <h4 class="category-title">Catégorie</h4>
+                    <div class="category">
+                        <div>
+                            <input type="radio" id="category-checkbox" name="radio" value="CTF" checked>
+                            <label for="CTF">CTF</label>
+                        </div>
+
+                        <div>
+                            <input type="radio" id="category-checkbox" name="radio" value="Tutorial" checked>
+                            <label for="Tutorial">Tutoriel</label>
+                        </div>
+                                            
+                        <div>
+                            <input type="radio" id="category-checkbox" name="radio" value="Project" checked>
+                            <label for="Project">Projet</label>
+                        </div>
+                    </div>
+                    <hr>
+                        
             </div>
         </div>
-
     </div>
+    <script src="../JS/tinymce.js"></script>
 </body>
 
 </html>
